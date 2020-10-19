344cf898ab1a012fe8d4bb095c4fc711d9d177fb
zurijang <wkdwn05@gmail.com>
2020-10-19 23:54:56 +0900

    remove recommend

--- recommend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Data
-
-class DataAdmin(admin.ModelAdmin):
-    list_display = ['date', 'state', 'province', 'name', 'type1', 'type2', 'type3', 'link']
-    search_fields = ['state', 'province', 'name', 'type1', 'type2', 'type3']
-
-admin.site.register(Data, DataAdmin)
